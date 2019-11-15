5edda06b668ef048bc5a0d2cf83a2cf12125b576
jyash97 <jyash97@gmail.com>
2019-11-15 15:00:50 +0530

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Minimal React Boilerplate
+
+Fully customizable React boilerplate, using latest `React`, `webpack` and `babel` version. It includes production build process + local development using `webpack-dev-server` with hot reload.
+
+## How to use?
+
+- `git clone https://github.com/lakhansamani/react-minimal-boilerplate.git`
+- [Optional] `mv react-minimal-boilerplate PROJECT_NAME`
+- `cd react-minial-boilerplate` or `cd PROJECT_NAME`
+
+### Development
+
+- `yarn && yarn start`
+
+### Production
+
+- `yarn build`
+- Production build will be part of `dist` folder
+
+### Testing
+
+- `yarn test`
+- If you change content of `src/App.js` then you might have to change the test in `src/App.test.js`. Also similary you can add test for more components.
+
+### How to add env variables to the application
+
+- `cp .env.sample .env`
+- There is a sample env variable declared there, you can replace that and add more env variables.
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WriteWebPackPlugin = require('write-file-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+const { NODE_ENV } = process.env;
+
+const plugins = [
+	// Ignore all locale files of moment.js
+	new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+	new CleanWebpackPlugin({
+		cleanOnceBeforeBuildPatterns: ['dist/app'],
+	}),
+	new HtmlWebpackPlugin({
+		template: './public/index.html',
+		alwaysWriteToDisk: true,
+	}),
+	new HtmlWebpackHarddiskPlugin(),
+	new CopyWebpackPlugin(['./public/favicon.ico', './public/manifest.json']),
+	new WriteWebPackPlugin(),
+	new Dotenv(),
+];
+
+const isDevelopment = NODE_ENV === 'development';
+
+module.exports = {
+	mode: NODE_ENV || isDevelopment,
+	entry: ['./src/index'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+		filename: isDevelopment ? '[name].js' : '[name].[chunkhash:8].js',
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js'],
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'initial',
+		},
+	},
+	plugins,
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader',
+						options: {
+							insertAt: 'top',
+						},
+					},
+					'css-loader',
+				],
+			},
+			{
+				test: /\.svg$/,
+				use: [
+					'@svgr/webpack',
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(gif|png|jpe?g)$/i,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+						},
+					},
+				],
+			},
+		],
+	},
+};
